dc7ac83e0c0fff800a5e269dc099b01c526d52ac
Jareth MacArthur <jtmaca@gmail.com>
2024-08-28 16:08:03 +1000

    test

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,45 +1,45 @@
-"use client";
+// "use client";
 
-import { auth } from "../services/firebase";
-import { User } from "firebase/auth";
-import {
-  createContext,
-  useContext,
-  useState,
-  ReactNode,
-  useEffect,
-} from "react";
+// import { auth } from "../services/firebase";
+// import { User } from "firebase/auth";
+// import {
+//   createContext,
+//   useContext,
+//   useState,
+//   ReactNode,
+//   useEffect,
+// } from "react";
 
-interface AuthContextType {
-  user: User | null;
-}
+// interface AuthContextType {
+//   user: User | null;
+// }
 
-const AuthContext = createContext<AuthContextType | undefined>(undefined);
+// const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
-export const useAuth = () => {
-  const context = useContext(AuthContext);
-  if (context === undefined) {
-    throw new Error("useAuth must be used within an AuthProvider");
-  }
-  return context;
-};
+// export const useAuth = () => {
+//   const context = useContext(AuthContext);
+//   if (context === undefined) {
+//     throw new Error("useAuth must be used within an AuthProvider");
+//   }
+//   return context;
+// };
 
-export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [ready, setReady] = useState(false);
-  const [user, setUser] = useState<User | null>(null);
+// export const AuthProvider = ({ children }: { children: ReactNode }) => {
+//   const [ready, setReady] = useState(false);
+//   const [user, setUser] = useState<User | null>(null);
 
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      setUser(user);
-      setReady(true);
-    });
+//   useEffect(() => {
+//     const unsubscribe = auth.onAuthStateChanged((user) => {
+//       setUser(user);
+//       setReady(true);
+//     });
 
-    return () => unsubscribe();
-  }, []);
+//     return () => unsubscribe();
+//   }, []);
 
-  return (
-    <AuthContext.Provider value={{ user }}>
-      {ready && children}
-    </AuthContext.Provider>
-  );
-};
+//   return (
+//     <AuthContext.Provider value={{ user }}>
+//       {ready && children}
+//     </AuthContext.Provider>
+//   );
+// };
