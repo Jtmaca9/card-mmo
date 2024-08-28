1c9820d3dec559f1a8b941f7a91535e4bd9d2349
Jareth MacArthur <jtmaca@gmail.com>
2024-08-29 08:51:23 +1000

    Auth working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,32 @@
 "use client";
 
-import { useAuth } from "@/context/AuthContext"; // Adjust the path as necessary
 import { useRouter } from "next/navigation";
+import { useUser } from "@auth0/nextjs-auth0/client";
 import { useEffect } from "react";
-import { signOut } from "firebase/auth";
-import { auth } from "@/services/firebase"; // Adjust the path as necessary
 
 export default function Home() {
-  const { user } = useAuth();
   const router = useRouter();
+  const { user, error, isLoading } = useUser();
 
   useEffect(() => {
-    if (!user) {
-      router.push("/login"); // Redirect to login if user is not authenticated
+    if (!isLoading && !user) {
+      router.push("/api/auth/login");
     }
-  }, [user, router]);
+  }, [isLoading, user]);
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100 text-black">
-      Home screen - {user?.email}
-      <button onClick={() => signOut(auth)} className="ml-4">
-        sign out
-      </button>
+      Home screen
+      {user && (
+        <>
+          <img src={user?.picture || ""} alt={user?.name} />
+          <h2>{user?.name}</h2>
+          <p>{user?.email}</p>
+          <button onClick={() => router.push("/api/auth/logout")}>
+            Logout
+          </button>
+        </>
+      )}
     </div>
   );
 }
