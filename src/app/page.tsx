f48a966512e2776281f41219750ca62687d411f7
Jareth MacArthur <jtmaca@gmail.com>
2024-08-28 16:12:55 +1000

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,27 @@
 "use client";
 
-// import { useAuth } from "@/context/AuthContext"; // Adjust the path as necessary
+import { useAuth } from "@/context/AuthContext"; // Adjust the path as necessary
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "@/services/firebase"; // Adjust the path as necessary
 
 export default function Home() {
-  // const { user } = useAuth();
+  const { user } = useAuth();
   const router = useRouter();
 
-  // useEffect(() => {
-  //   if (!user) {
-  //     router.push("/login"); // Redirect to login if user is not authenticated
-  //   }
-  // }, [user, router]);
+  useEffect(() => {
+    if (!user) {
+      router.push("/login"); // Redirect to login if user is not authenticated
+    }
+  }, [user, router]);
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100 text-black">
-      Home screen - user email
-      {/* <button onClick={() => signOut(auth)} className="ml-4">
+      Home screen - {user?.email}
+      <button onClick={() => signOut(auth)} className="ml-4">
         sign out
-      </button> */}
+      </button>
     </div>
   );
 }
